ce2e8d27acf0f5f4aa122db5962f3f3a31a25694
Corwin Joy <corwin.joy@gmail.com>
2025-05-20 21:34:07 -0700

    Upgrade code to support varying number of rows.

--- crates/deltalake/examples/benchmark.rs
+++ crates/deltalake/examples/benchmark.rs
@@ -9,13 +9,13 @@ use deltalake::operations::collect_sendable_stream;
 use deltalake::parquet::{
     file::properties::WriterProperties,
 };
-use deltalake::{parquet, DeltaOps};
+use deltalake::{DeltaOps};
 
 use std::sync::Arc;
 use futures::StreamExt;
 use deltalake::datafusion::execution::runtime_env::RuntimeEnv;
 use deltalake::datafusion::execution::{SessionState, SessionStateBuilder};
-use deltalake::datafusion::prelude::{SessionConfig, SessionContext};
+use deltalake::datafusion::prelude::{SessionConfig};
 use deltalake::parquet::encryption::decrypt::FileDecryptionProperties;
 use deltalake::parquet::encryption::encrypt::FileEncryptionProperties;
 use deltalake_core::datafusion::config::ConfigFileDecryptionProperties;
@@ -23,40 +23,76 @@ use deltalake_core::{DeltaTable, DeltaTableError};
 use deltalake_core::logstore::LogStoreRef;
 use url::Url;
 use deltalake::arrow::datatypes::Schema;
-use std::time::{Duration, Instant};
-use deltalake::arrow::array::Float32Array;
+use std::time::{Instant};
+use rand::{Rng};
+use deltalake::arrow::array::{Array, Float32Array};
 
-fn get_table_columns() -> Vec<StructField> {
-    vec![
-        StructField::new(
-            String::from("float"),
-            DataType::Primitive(PrimitiveType::Float),
-            false,
-        ),
-    ]
+
+fn get_column_names(ncol: usize) -> Vec<String> {
+    let mut colnames = Vec::new(); // Initialize an empty vector to hold the strings.
+
+    for i in 0..ncol {
+        let colname = format!("flt{}", i);
+        colnames.push(colname);
+    }
+    colnames
 }
 
-fn get_table_schema() -> Arc<Schema> {
-    let schema = Arc::new(ArrowSchema::new(vec![
-        Field::new("float", ArrowDataType::Float32, false),
-    ]));
+fn get_table_columns(ncol: usize) -> Vec<StructField> {
+    let mut cols = Vec::new(); // Initialize an empty vector to hold the strings.
+    let colnames = get_column_names(ncol);
+
+    for i in 0..ncol {
+        let col = StructField::new(
+            colnames[i].clone(),
+            DataType::Primitive(PrimitiveType::Float),
+            false,
+        );
+        cols.push(col); // Add the new String to the vector.
+    }
+
+    cols
+}
+
+fn get_table_schema(ncol: usize) -> Arc<Schema> {
+    let mut fields = Vec::new();
+    let colnames = get_column_names(ncol);
+    for i in 0..ncol {
+        let field = Field::new(
+            colnames[i].clone(),
+            ArrowDataType::Float32,
+            false
+
+        );
+        fields.push(field); // Add the new String to the vector.
+    }
+    let schema = Arc::new(ArrowSchema::new(fields));
     schema
 }
 
-fn get_table_batches() -> RecordBatch {
-    let schema = get_table_schema();
+fn get_table_batches(ncol: usize, nrow: usize) -> RecordBatch {
+    let mut rng = rand::rng();
+    let schema = get_table_schema(ncol);
 
-    let float_values = Float32Array::from(vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0]);
-    RecordBatch::try_new(
+    let mut columns: Vec<Arc<dyn Array>> = Vec::new();
+    for _i in 0..ncol {
+        let mut vals: Vec<f32> = Vec::with_capacity(nrow);
+        for _j in 0..nrow {
+            vals.push(rng.random());
+        }
+        let float_values = Float32Array::from(vals);
+        columns.push(Arc::new(float_values));
+    }
+
+    let batches = RecordBatch::try_new(
         schema,
-        vec![
-            Arc::new(float_values),
-        ],
-    )
-    .unwrap()
+        columns,
+    ).unwrap();
+
+    batches
 }
 
-async fn create_table(uri: &str, table_name: &str, crypt: &FileEncryptionProperties) -> Result<DeltaTable, DeltaTableError> {
+async fn create_table(uri: &str, table_name: &str, crypt: &FileEncryptionProperties, ncol: usize, nrow: usize) -> Result<DeltaTable, DeltaTableError> {
     fs::remove_dir_all(uri)?;
     let ops = DeltaOps::try_from_uri(uri).await?;
 
@@ -65,7 +101,7 @@ async fn create_table(uri: &str, table_name: &str, crypt: &FileEncryptionPropert
     // options are set you can run the command using `.await`.
     let table = ops
         .create()
-        .with_columns(get_table_columns())
+        .with_columns(get_table_columns(ncol))
         .with_table_name(table_name)
         .with_comment("A table to show how delta-rs works")
         .await?;
@@ -76,7 +112,7 @@ async fn create_table(uri: &str, table_name: &str, crypt: &FileEncryptionPropert
         .with_file_encryption_properties(crypt.clone())
         .build();
 
-    let batch = get_table_batches();
+    let batch = get_table_batches(ncol, nrow);
     let table = DeltaOps(table)
         .write(vec![batch.clone()])
         .with_writer_properties(writer_properties.clone())
@@ -112,19 +148,19 @@ async fn read_table(uri: &str, decryption_properties: &FileDecryptionProperties)
 
     let (_table, mut stream) = DeltaOps(table).load()
         .with_session_state(state)
-        .with_columns(vec!["float"]) // only read specified columns
+        .with_columns(vec!["flt0"]) // only read specified columns
         .await?;
-    
+
     /*
     // Read and print full data
     let data: Vec<RecordBatch> = collect_sendable_stream(stream).await?;
     println!("{data:?}");
-    
+
     */
 
     // Read data, streaming and discarding record batches:
     let mut batch_count = 0;
-    while let Some(item) = stream.next().await {
+    while let Some(_item) = stream.next().await {
         batch_count = batch_count + 1;
     }
 
@@ -137,18 +173,24 @@ async fn round_trip_test() -> Result<(), deltalake::errors::DeltaTableError> {
     let uri = "/home/cjoy/src/delta-rs-with-encryption/delta-rs/crates/deltalake/examples/encrypted_roundtrip";
     let table_name = "roundtrip";
     let key: Vec<_> = b"1234567890123450".to_vec();
+    let ncol: usize = 5;
+    let nrow: usize = 20;
+    let colnames = get_column_names(ncol);
 
-    let crypt = parquet::encryption::encrypt::
-        FileEncryptionProperties::builder(key.clone())
-            .with_column_key("float", key.clone())
-            .build()?;
+    let mut crypt_builder = FileEncryptionProperties::builder(key.clone());
+    for i in 0..ncol {
+        crypt_builder = crypt_builder.with_column_key(&*colnames[i], key.clone());
+    }
+    let crypt = crypt_builder.build()?;
 
-    let decrypt = FileDecryptionProperties::builder(key.clone())
-        .with_column_key("float", key.clone())
-        .build()?;
+    let mut decrypt_builder = FileDecryptionProperties::builder(key.clone());
+    for i in 0..ncol {
+        decrypt_builder = decrypt_builder.with_column_key(&*colnames[i], key.clone());
+    }
+    let decrypt = decrypt_builder.build()?;
 
     let start = Instant::now();
-    create_table(uri, table_name, &crypt).await?;
+    create_table(uri, table_name, &crypt, ncol, nrow).await?;
     let duration = start.elapsed();
     println!("Time elapsed in create_table() is: {:?}", duration);
 
