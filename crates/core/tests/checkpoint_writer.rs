51bfbf3f44c5a47be08d39e4df06abf1002e648b
Corwin Joy <corwin.joy@gmail.com>
2025-09-09 21:14:21 -0700

    clarify logic. Remove unused includes.
    Signed-off-by: Corwin Joy <corwin.joy@gmail.com>

--- crates/core/tests/checkpoint_writer.rs
+++ crates/core/tests/checkpoint_writer.rs
@@ -219,8 +219,8 @@ mod delete_expired_delta_log_in_checkpoint {
 
         // set last_modified
         set_file_last_modified(0, 25 * 60 * 1000); // 25 mins ago, should be deleted
-        set_file_last_modified(1, 15 * 60 * 1000); // 25 mins ago, should be deleted
-        set_file_last_modified(2, 5 * 60 * 1000); // 25 mins ago, should be kept
+        set_file_last_modified(1, 15 * 60 * 1000); // 15 mins ago, should be deleted
+        set_file_last_modified(2, 5 * 60 * 1000); // 5 mins ago, should be kept
 
         table.load_version(0).await.expect("Cannot load version 0");
         table.load_version(1).await.expect("Cannot load version 1");
@@ -263,7 +263,8 @@ mod delete_expired_delta_log_in_checkpoint {
 
     #[tokio::test]
     async fn test_delete_expired_logs_safe_checkpoint() {
-        println!("test_delete_expired_logs_safe_checkpoint");
+        // For additional tracing:
+        // let _ = pretty_env_logger::try_init();
         let mut table = fs_common::create_table(
             "../test/tests/data/checkpoints_with_expired_logs/expired",
             Some(hashmap! {
@@ -287,17 +288,25 @@ mod delete_expired_delta_log_in_checkpoint {
         // create 2 commits
         let a1 = fs_common::add(0);
         let a2 = fs_common::add(0);
+        let a3 = fs_common::add(0);
+        let a4 = fs_common::add(0);
         assert_eq!(1, fs_common::commit_add(&mut table, &a1).await);
         assert_eq!(2, fs_common::commit_add(&mut table, &a2).await);
+        assert_eq!(3, fs_common::commit_add(&mut table, &a3).await);
+        assert_eq!(4, fs_common::commit_add(&mut table, &a4).await);
 
         // set last_modified
         set_file_last_modified(0, 25 * 60 * 1000, "json"); // 25 mins ago, should be deleted
-        set_file_last_modified(1, 15 * 60 * 1000, "json"); // 25 mins ago, fails retention cutoff, but kept due to version
-        set_file_last_modified(2, 5 * 60 * 1000, "json"); // 25 mins ago, should be kept
+        set_file_last_modified(1, 20 * 60 * 1000, "json"); // 20 mins ago, last safe checkpoint, should be kept
+        set_file_last_modified(2, 15 * 60 * 1000, "json"); // 15 mins ago, fails retention cutoff, but needed by v3
+        set_file_last_modified(3, 6 * 60 * 1000, "json"); // 6 mins ago, should be kept by retention cutoff
+        set_file_last_modified(4, 5 * 60 * 1000, "json"); // 5 mins ago, should be kept by retention cutoff
 
         table.load_version(0).await.expect("Cannot load version 0");
         table.load_version(1).await.expect("Cannot load version 1");
         table.load_version(2).await.expect("Cannot load version 2");
+        table.load_version(3).await.expect("Cannot load version 3");
+        table.load_version(4).await.expect("Cannot load version 4");
 
         // Create checkpoint for version 1
         checkpoints::create_checkpoint_from_table_uri_and_cleanup(
@@ -310,7 +319,7 @@ mod delete_expired_delta_log_in_checkpoint {
         .unwrap();
 
         // Update checkpoint time for version 1 to be just after version 1 data
-        set_file_last_modified(1, 15 * 60 * 1000 - 10, "checkpoint.parquet");
+        set_file_last_modified(1, 20 * 60 * 1000 - 10, "checkpoint.parquet");
 
         checkpoints::create_checkpoint_from_table_uri_and_cleanup(
             deltalake_core::ensure_table_uri(&table.table_uri()).unwrap(),
@@ -329,20 +338,16 @@ mod delete_expired_delta_log_in_checkpoint {
                 .file_paths_iter()
                 .collect::<Vec<_>>(),
             vec![
+                ObjectStorePath::from(a4.path.as_ref()),
+                ObjectStorePath::from(a3.path.as_ref()),
                 ObjectStorePath::from(a2.path.as_ref()),
                 ObjectStorePath::from(a1.path.as_ref()),
             ]
         );
 
-        // log file 0 is deleted
-        table
-            .load_version(0)
-            .await
-            .expect_err("Should not load version 0");
-
-        // log file 1 and 2 are kept
-        table.load_version(1).await.expect("Cannot load version 1");
-        table.load_version(2).await.expect("Cannot load version 2");
+        // Without going back to a safe checkpoint, loading version 3 would fail..
+        table.load_version(3).await.expect("Cannot load version 3");
+        table.load_version(4).await.expect("Cannot load version 4");
     }
 
     #[tokio::test]
