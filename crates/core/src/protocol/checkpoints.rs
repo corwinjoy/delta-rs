3f782ffddca8cb4f40df6a28a008312c0595da1d
Corwin Joy <corwin.joy@gmail.com>
2025-09-11 20:44:00 -0700

    Cleaup comment
    Signed-off-by: Corwin Joy <corwin.joy@gmail.com>

--- crates/core/src/protocol/checkpoints.rs
+++ crates/core/src/protocol/checkpoints.rs
@@ -213,7 +213,7 @@ pub async fn cleanup_expired_logs_for(
     let object_store = log_store.object_store(None);
     let log_path = log_store.log_path();
 
-    // Step 0: require presence of _last_checkpoint for safety like previous impl
+    // Step 0: require presence of _last_checkpoint for safety
     let maybe_last_checkpoint = read_last_checkpoint(&object_store, log_path).await?;
     if maybe_last_checkpoint.is_none() {
         debug!("no last checkpoint. Exiting cleanup_expired_logs_for");
